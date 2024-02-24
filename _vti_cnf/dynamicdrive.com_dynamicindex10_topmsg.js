ef52a90b5503245e375482bb62c1376418b48525
D Gartland <dgartland021@c2ken.net>
2024-02-24 18:27:09 +0000

    Initial commit of website files

---
+++ _vti_cnf/dynamicdrive.com_dynamicindex10_topmsg.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|22 Nov 2023 10:15:00 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|C2KEN\\omckay632
+vti_modifiedby:SR|C2KEN\\omckay632
+vti_timecreated:TR|19 Oct 2023 10:59:59 -0000
+vti_backlinkinfo:VX|
+vti_cacheddtm:TX|22 Nov 2023 10:15:00 -0000
+vti_filesize:IR|3364
---
+++ _vti_cnf/dynamicdrive.com_dynamicindex9_fluidtextresizer.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|22 Nov 2023 09:53:24 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|C2KEN\\omckay632
+vti_modifiedby:SR|C2KEN\\omckay632
+vti_timecreated:TR|22 Nov 2023 09:53:24 -0000
+vti_cacheddtm:TX|22 Nov 2023 09:53:24 -0000
+vti_filesize:IR|3948
+vti_backlinkinfo:VX|
